88f124bec9adf0c1bde65e65d13a94ddba7142a5
MiguelCarreras <carreras69miguel@gmail.com>
2023-02-27 23:51:23 -0300

    Redirect to home with icon

--- client/src/layouts/PageHeader.jsx
+++ client/src/layouts/PageHeader.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import { useNavigate } from 'react-router-dom';
 
-const PageHeader = ({ goToHome }) => {
+const PageHeader = () => {
+    const navigate = useNavigate();
+    
+    const goToHome = () => {
+        navigate('/');
+    }
+    
     return (
         <div className='row page-header'>
             <div className='col-md-10 pull-left'>
-                <span className='page-header-icon'>
+                <span className='page-header-icon' onClick={goToHome}>
                     <FontAwesomeIcon icon={solid('otter')} />
                 </span>
-                <span className='page-header-title'> Pet Shelter</span>
+                <span className='page-header-title' onClick={goToHome}> Pet Shelter</span>
             </div>
         </div>
     );
