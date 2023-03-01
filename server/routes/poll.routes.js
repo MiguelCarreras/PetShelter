3c85b930d4e76f925705f752c9f684da39156d3e
MiguelCarreras <carreras69miguel@gmail.com>
2023-02-28 22:37:00 -0300

    Remove unused files from server

--- server/routes/poll.routes.js
+++
@@ -1,5 +0,0 @@
-const PollController = require('../controllers/poll.controller');
-module.exports = function(app){
-    app.get('/api', PollController.index);
-    app.post('/api/poll', PollController.create);
-}
