92093a264c0fe2d30723e73c216629539901b748
MiguelCarreras <carreras69miguel@gmail.com>
2023-02-28 23:26:49 -0300

    Added validations to pet model

--- server/models/pet.model.js
+++ server/models/pet.model.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 const PetSchema = new mongoose.Schema({
-    name: { type: String },
-    type: { type: String },
-    description: { type: String },
+    name: { type: String,
+            required: true,
+            minLength: 3 },
+    type: { type: String,
+            required: true,
+            minLength: 3 },
+    description: { type: String,
+                   required: true,
+                   minLength: 3 },
     skill1: { type: String },
     skill2: { type: String },
     skill3: { type: String },
-    likes: { type: Number, default: 0 } 
+    likes: { type: Number, 
+             default: 0,
+             required: true,
+             min: 0 } 
 }, { timestamps: true });
 module.exports.Pet = mongoose.model('Pet', PetSchema);
-
